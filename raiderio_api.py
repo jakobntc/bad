bbd11aa602b96b0491e1e0972bb013fc76eab03e
Jakob Clapsaddle <Jakobntc.95@gmail.com>
2022-09-03 02:09:47 -0400

    Chaning output.

--- raiderio_api.py
+++ raiderio_api.py
@@ -15,8 +15,9 @@ def get_data(url: str):
 def get_character(realm: str, name: str) -> Character:
     r = requests.get(f"{BASE_URL}region={REGION}&realm={realm}&name={name}")
     if r.status_code == 200:
-        char = Character.parse_raw(r.text)
-        return char
+        pprint(r.json())
+        # char = Character.parse_raw(r.text)
+        # return char
     else:
         print(f"Hello person, there's a {r.status_code} error with your request")
 
@@ -24,4 +25,4 @@ def get_character(realm: str, name: str) -> Character:
 
 if __name__ == "__main__":
     # get_data(url=BASE_URL)
-    pprint(get_character("tichondrius", "saddledk"))
+    get_character("tichondrius", "saddledk")
